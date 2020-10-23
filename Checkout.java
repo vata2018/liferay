397e3a024f12ff5c6abee53dcdcee1c20a87dc33
vata2018 <70289837+vata2018@users.noreply.github.com>
2020-10-22 20:42:46 -0500

    Cleaner checkout

--- Checkout.java
+++ Checkout.java
@@ -1,19 +1,31 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Checkout {
 
-  public static void main(String[] args) {
-    Receipt receipt = new Receipt();
-    Scanner sc = new Scanner(System.in);
-    String line = sc.nextLine();
-    
-    while (line.length() > 0) {
-      receipt.addItem(line);
-      line = sc.nextLine();
-    }
+	public static void main(String[] args) {
 
-    System.out.println(Format.formatReceipt(receipt));
-    sc.close();
-  }
+		try {
+			File fromFile = new File("inputfiles/" + args[0]);
+			BufferedReader in = new BufferedReader(new FileReader(fromFile));
 
+			Receipt receipt = new Receipt();
+			String line = in.readLine();
+
+			while (line != null) {
+				receipt.addItem(line);
+				line = in.readLine();
+			}
+
+			System.out.println(Format.formatReceipt(receipt));
+			in.close();
+
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
+
